188b4849e2ce672cc7d046eb059d4360734afec9
Brendan Martin <brendan.martin@ring.com>
2017-05-09 12:50:22 -0400

    Adding mocha and lodash

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 const calc = require('./calc');
+const _ = require('lodash');
 
 const num = [
   2,3,4
 ];
 
 console.log(`The result is: ${calc.sum(num)}`);
+console.log(_.assign({'a': 1},{'b': 2}));
